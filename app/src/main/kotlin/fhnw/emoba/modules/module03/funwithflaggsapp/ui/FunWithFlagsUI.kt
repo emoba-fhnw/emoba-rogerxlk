33e8e736bf34e98adc4e79e0469966609f974079
Roger Sennhauser <roger.sennhauser@students.fhnw.ch>
2022-10-05 18:37:42 +0200

    ck: 120min - working on FunWithFlagsUI (had to do it twice as I mistakenly pressed rollback)

--- app/src/main/kotlin/fhnw/emoba/modules/module03/funwithflaggsapp/ui/FunWithFlagsUI.kt
+++ app/src/main/kotlin/fhnw/emoba/modules/module03/funwithflaggsapp/ui/FunWithFlagsUI.kt
@@ -2,52 +2,120 @@ package fhnw.emoba.modules.module03.funwithflaggsapp.ui
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fhnw.emoba.R
+import kotlinx.coroutines.launch
 
 @Composable
 fun FunWithFlagsUI(title: String) {
-    Column() {
-        TopBar(title = title)
-        SheldonImageBox()
+    Column {
+        MyScaffold(title = title)
     }
-}
-
-@Composable
-private fun TopBar(title: String) {
-    TopAppBar(title = {
-        Text(
-            text = title,
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(5.dp)
-        )
-    })
 }
 
 @Composable
 private fun SheldonImageBox() {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()) {
+        modifier = Modifier.fillMaxSize()
+    ) {
         ImageWithBlockingUI(resId = R.drawable.sheldon, description = "Sheldon")
     }
 }
 
 @Composable
+private fun CzeckImage() {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        ImageWithBlockingUI(resId = R.drawable.tschechien, description = "Flag of Czeck Republic")
+    }
+}
+
+@Composable
+private fun SlovakiaImage() {
+
+    Box(
+        contentAlignment = Alignment.TopCenter,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+
+    ) {
+        ImageWithBlockingUI(resId = R.drawable.slowakei, description = "Flag of Slovakia")
+    }
+}
+
+@Composable
 private fun ImageWithBlockingUI(@DrawableRes resId: Int, description: String) {
-    Image(painter = painterResource(id = resId),
-        contentDescription = description)
+    Image(
+        painter = painterResource(id = resId),
+        contentDescription = description,
+    )
+}
+
+@Composable
+private fun MyScaffold(title: String) {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = { SheldonImageBox() },
+        topBar = {
+            TopAppBar(
+                backgroundColor = Color(156, 166, 143),
+                title = {
+                    Text(
+                        text = title,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                },
+
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            scope.launch { scaffoldState.drawerState.open() }
+                        }
+                    ) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+                    }
+                }
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(contentPadding = innerPadding) {}
+            Column {
+                SlovakiaImage()
+                Spacer(modifier = Modifier.fillMaxHeight(0.74f))
+                CzeckImage()
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    FunWithFlagsUI("")
+
 }
